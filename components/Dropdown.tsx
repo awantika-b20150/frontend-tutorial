775687199ec5a727443e29c94c1df18e9ceebe7a
awantika-deora <awantika.official@gmail.com>
2024-12-06 08:17:00 +0900

    added search functionality in the dropdown menu

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { DownOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Dropdown, message, Space } from 'antd';
+import { Select } from 'antd';
 import Prefectures from "@/components/Pref";
 
-const onClick: MenuProps['onClick'] = ({ key }) => {
-  console.log(`Click on ${key}`);
+const onClick = (value: string) => {
+  console.log(`Click on ${value}`);
+};
+const onChange = (value: string) => {
+  console.log(`selected ${value}`);
 };
 
-const items: MenuProps['items'] = Prefectures;
+const onSearch = (value: string) => {
+  console.log('search:', value);
+};
 
 const Menu: React.FC = () => (
-  <Dropdown menu={{ items, onClick }}>
-    <a onClick={(e) => e.preventDefault()}>
-      <Space>
-        Select Prefecture
-        <DownOutlined />
-      </Space>
-    </a>
-  </Dropdown>
+  <Select
+    showSearch
+    placeholder="Select prefecture"
+    optionFilterProp="label"
+    onChange={onChange}
+    onSearch={onSearch}
+    options={Prefectures}
+  />
 );
 
 export default Menu;
